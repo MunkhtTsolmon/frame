096d06930b0a98efa415b7125756ce6a0553a6d4
MunkhtTsolmon <monkhtsolmonmuzo@gmail.com>
2024-11-25 20:03:20 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,131 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { LuPhone } from "react-icons/lu";
+import { CiMail } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
+const arr = [
+  {
+    id: 1,
+    firstName: "Marian",
+    lastName: "Dixsee",
+    email: "mdixsee0@nba.com",
+    companyName: "Blogtags",
+    phoneNumber: "412-530-2249",
+    address: "62 Meadow Ridge Plaza",
+  },
+  {
+    id: 2,
+    firstName: "Coleman",
+    lastName: "Oxbie",
+    email: "coxbie1@oaic.gov.au",
+    companyName: "Fatz",
+    phoneNumber: "592-295-2012",
+    address: "18 Macpherson Place",
+  },
+  {
+    id: 3,
+    firstName: "Allx",
+    lastName: "Tomasian",
+    email: "atomasian2@cafepress.com",
+    companyName: "Abata",
+    phoneNumber: "741-828-1962",
+    address: "42040 Ridgeway Trail",
+  },
+  {
+    id: 4,
+    firstName: "Jarrett",
+    lastName: "Randales",
+    email: "jrandales3@wiley.com",
+    companyName: "Vidoo",
+    phoneNumber: "401-321-9934",
+    address: "595 Lakewood Street",
+  },
+  {
+    id: 5,
+    firstName: "Mora",
+    lastName: "Tildesley",
+    email: "mtildesley4@privacy.gov.au",
+    companyName: "Shuffletag",
+    phoneNumber: "254-913-7925",
+    address: "7 Little Fleur Point",
+  },
+  {
+    id: 6,
+    firstName: "Livvyy",
+    lastName: "Crankhorn",
+    email: "lcrankhorn5@google.com.br",
+    companyName: "Fivespan",
+    phoneNumber: "589-951-2909",
+    address: "23524 Thackeray Street",
+  },
+  {
+    id: 7,
+    firstName: "Erwin",
+    lastName: "LLelweln",
+    email: "ellelweln6@de.vu",
+    companyName: "Cogibox",
+    phoneNumber: "105-760-3548",
+    address: "20 Cardinal Pass",
+  },
+  {
+    id: 8,
+    firstName: "Jourdain",
+    lastName: "Leban",
+    email: "jleban7@is.gd",
+    companyName: "Layo",
+    phoneNumber: "124-860-5889",
+    address: "77 Fair Oaks Court",
+  },
+  {
+    id: 9,
+    firstName: "Christy",
+    lastName: "Van Der Straaten",
+    email: "cvanderstraaten8@dailymail.co.uk",
+    companyName: "Linktype",
+    phoneNumber: "460-169-0338",
+    address: "94749 Mendota Circle",
+  },
+  {
+    id: 10,
+    firstName: "Jehanna",
+    lastName: "Cleobury",
+    email: "jcleobury9@huffingtonpost.com",
+    companyName: "Flashpoint",
+    phoneNumber: "396-811-4137",
+    address: "3759 Oak Valley Park",
+  },
+];
 export default function Home() {
+  let newArr = arr.map((person) => (
+    <div className="frame" key={person.id}>
+      <div className="name">
+        <h3>{person.firstName}</h3>
+        <h3>{person.lastName}</h3>
+      </div>
+      <div className="footer">
+        <p>
+          <LuPhone />
+          {person.phoneNumber}
+        </p>
+        <p>
+          <CiMail />
+          {person.email}
+        </p>
+        <p>
+          <CiLocationOn />
+          {person.address}
+        </p>
+      </div>
+      <div className="company">
+        <Image src="/Logo.jpg" alt="Company Logo" width={30} height={30} />
+        <h3>{person.companyName}</h3>
+      </div>
+    </div>
+  ));
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="container" style={{}}>
+      {newArr}
     </div>
   );
 }
